f589f64317a3d9b9f26385c9f615468fe09cf33e
benjjo <benjjo@gmail.com>
2020-04-20 09:26:58 +0100

    Gotta give out the love where the love is due.

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,12 @@
 
 import java.util.concurrent.ThreadLocalRandom;
 
+/**
+ * Generates a random set of keyboard characters to the length of the specified argument.
+ *
+ * Thanks to: https://stackoverflow.com/questions/363681/how-do-i-generate-random-integers-within-a-specific-range-in-java
+ */
+
 public class Main {
 
     public static void main(String[] args){
