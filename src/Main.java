826f3abaa0b157a0c038e12b1cc90ceedfff33f0
benjjo <benjjo@gmail.com>
2020-04-27 11:50:56 +0100

    Now with icons

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.net.URL;
 
 /**
  * Generates a random set of keyboard characters to the length of the specified argument.
@@ -17,6 +21,19 @@ public class Main {
             JFrame frame = new MainFrame("Password GEN");
             frame.setSize(266, 270); // sets the size of the frame
             frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // sets up JFrame to close when cancelled.
+
+            // Setup the icon
+            try {
+                URL resource = frame.getClass().getResource("./password-icon-5.png");
+                BufferedImage image = ImageIO.read(resource);
+                frame.setIconImage(image);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            // Display the form
+            frame.setVisible(true);
+
             frame.setVisible(true); // Draws the frame to the screen
         }});
     }
